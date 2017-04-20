6fb41562304d95e99a74028dd4f1aa8ea26aa487
MestDoranbolt <karol.wojciula95@gmail.com>
2017-04-21 00:53:56 +0200

    Admin (login: admin password: djangoadmin) and not working new views ;/

--- Doggo/urls.py
+++ Doggo/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # ex: /Doggo/details
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
     url(r'^P<Kandydat_id>[0-9]+/$', views.details, name='details'),
     url(r'^P<Kandydat_id>[0-9]+/results/$', views.results, name='results'),
 ]
--- Doggo/views.py
+++ Doggo/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Kandydat
 
@@ -12,7 +13,12 @@ def details(request, kandydat_id):
 def results(request, kandydat_id):
     return HttpResponse("Result for: " % kandydat_id)
 
-def index(request):
+
+def home(request):
     latest_question_list = Kandydat.objects.order_by('-imie')[:5]
-    output = ', '.join([str(q.id) for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('home.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # output = ', '.join([str(q.id) for q in latest_question_list])
+    return HttpResponse(template.render(context, request))

d6ec1861264eb57d772039127543d5b70fc1efff
dotrek <dotrek1920@gmail.com>
2017-05-17 16:03:46 +0200

    Fixed csrf error with removing csrf

--- Doggo/views.py
+++ Doggo/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.urls import reverse
-from django.views.decorators.csrf import csrf_protect
 
 from .models import Kandydat, Wybory
 
@@ -14,20 +13,19 @@ def results(request, kandydat_id):
     return render_to_response(request, 'results.html', { 'kandydat' : kandydat})
 
 
-@csrf_protect
 def vote(request, kandydat_id):
-    kandydat = get_object_or_404(Kandydat, pk=kandydat_id)
+
     try:
-        selected_choice = kandydat.get(pk=request.POST['choice'])
+        selected_choice = get_object_or_404(Kandydat, pk=kandydat_id)
     except (KeyError, Kandydat.DoesNotExist):
         return render_to_response(request, 'detail_wybory.html', {
-            'kandydat' : kandydat,
+            'kandydat': selected_choice,
             'error_message' : "Nie wybrałeś kandydata.",
         })
     else:
         selected_choice.licznik += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('home.html'))
+        return HttpResponseRedirect(reverse('Doggo:home'))
 
 
 def home(request):

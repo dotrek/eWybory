e6ac9aa52c47fff358924a4c34b4d25cc763b3ac
Marek Jakubowski <vertiavo@gmail.com>
2017-06-01 02:29:08 +0200

    Fixed vote view and adding new vote into database

--- Doggo/views.py
+++ Doggo/views.py
@@ -17,36 +17,34 @@ class ResultsView(generic.DetailView):
 def vote(request, wybory_id):
     wybory = get_object_or_404(Wybory, pk=wybory_id)
     kandydat_list = Kandydat.objects.filter(wybory_id=wybory_id).order_by('nazwisko')[:5]
-    context = {
-        'error_message': "Pesel nie znajduje sie w bazie glosujacych",
-    }
     try:
-
         selected_choice = get_object_or_404(Kandydat, pk=request.POST['choice'])
         user = Glosujacy.objects.get(pesel=request.POST['pesel'])
-    except (KeyError, Kandydat.DoesNotExist):
-        return render(request, 'detail_wybory.html',context)
-    except (UnboundLocalError, Glosujacy.DoesNotExist):
-        return render(request, 'detail_wybory.html', {
-            'wybory': wybory,
-            'kandydat_list': kandydat_list,
-            'error_message': "Pesel nie znajduje sie w bazie glosujacych",
-        })
-    else:
-        try:
-            query = Glos.objects.filter(wybory=wybory)
-            glos = get_object_or_404(query, glosujacy=user)
-        except glos.DoesNotExist:
-            glos.wybory = wybory
-            glos.glosujacy = user
+
+        glos = Glos.objects.filter(
+            wybory__exact=wybory
+        ).filter(
+            glosujacy__exact=user
+        )
+
+        if glos.count() == 0:
+            glos = Glos(wybory=wybory, glosujacy=user)
             glos.save()
             selected_choice.licznik += 1
             selected_choice.save()
             return HttpResponseRedirect(reverse('Doggo:results', args=(wybory.id,)))
         else:
             return render(request, 'detail_wybory.html', {
-            'error_message': "Ta osoba juz glosowala w tych wyborach"
+                'wybory': wybory,
+                'kandydat_list': kandydat_list,
+                'error_message': "Ta osoba juz glosowala w tych wyborach",
             })
+    except (KeyError, Glosujacy.DoesNotExist, Kandydat.DoesNotExist):
+        return render(request, 'detail_wybory.html', {
+            'wybory': wybory,
+            'kandydat_list': kandydat_list,
+            'error_message': "Pesel nie znajduje sie w bazie glosujacych",
+        })
 
 
 class HomeView(generic.ListView):
@@ -66,7 +64,7 @@ def detail_wybory(request, wybory_id):
     wybory = get_object_or_404(Wybory, pk=wybory_id)
     kandydat_list = Kandydat.objects.filter(wybory_id=wybory_id).order_by('nazwisko')[:5]
     liczba_dopuszczonych = Glosujacy.objects.all().count()
-    liczba_glosujacych = Glos.objects.filter(wybory_id=wybory_id).count()
+    liczba_glosujacych = Glos.objects.filter(wybory=wybory).count()
     frekwencja = liczba_glosujacych / liczba_dopuszczonych
 
     context = {
@@ -80,8 +78,12 @@ def detail_wybory(request, wybory_id):
 def results(request, wybory_id):
     wybory = get_object_or_404(Wybory, pk=wybory_id)
     kandydat_list = Kandydat.objects.filter(wybory_id=wybory_id).order_by('nazwisko')[:5]
+    liczba_dopuszczonych = Glosujacy.objects.all().count()
+    liczba_glosujacych = Glos.objects.filter(wybory_id=wybory_id).count()
+    frekwencja = liczba_glosujacych / liczba_dopuszczonych * 100
     context = {
         'wybory': wybory,
         'kandydat_list': kandydat_list,
+        'frekwencja': frekwencja
     }
     return render_to_response('results.html', context)
